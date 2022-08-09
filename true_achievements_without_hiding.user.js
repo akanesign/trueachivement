58ad6c5185ff05b5e9a26eeb7545d409706ede6b
akanesign <110755602+akanesign@users.noreply.github.com>
2022-08-09 15:40:50 +0900

    add achivement page

--- true_achievements_without_hiding.user.js
+++ true_achievements_without_hiding.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         TrueAchievements without hiding
-// @version      0.7
+// @version      0.8
 // @description  ARE YOU AN ACHIEVEMENT WHORE?
 // @author       akanesign
 // @match        https://www.trueachievements.com/game/*
 // @match        https://www.trueachievements.com/*/*?gamerid=*
 // @match        https://www.trueachievements.com/game/*/achievements
+// @match        https://www.trueachievements.com/*/*-achievements
 // @require      https://code.jquery.com/jquery-3.5.1.min.js
 // @updateURL    https://github.com/akanesign/trueachivement/raw/main/true_achievements_without_hiding.user.js
 // @downloadURL  https://github.com/akanesign/trueachivement/raw/main/true_achievements_without_hiding.user.js
